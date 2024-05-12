841ba61ea62b03cf9e469d1f051e4b59791a7535
Mario G <mariojgintili@gmail.com>
2024-05-12 18:53:16 +0100

    add datasets and do some extensions to the actual ds study

--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
 (defproject cashcloj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An exploration of the stock market using data science & ML tools"
+  :url "https://github.com/shellandbull/cashcloj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [scicloj/tablecloth "7.029.2"]
+                 [org.scicloj/clay "2-beta8"]
+                 [org.scicloj/noj "1-alpha34"]
+                 [com.cnuernber/charred "1.033"]
+                 [generateme/fastmath "2.4.0"]]
+  :resource-paths ["data"]
   :repl-options {:init-ns cashcloj.core})
--- src/cashcloj/core.clj
+++ src/cashcloj/core.clj
@@ -1,6 +1,57 @@
-(ns cashcloj.core)
+(ns cashcloj.core
+    (:require [scicloj.clay.v2.api :as clay]
+            [clojure.string :as str]
+            [tablecloth.api :as tc]
+            [scicloj.clay.v2.api :as clay]
+            [libpython-clj2.require :refer [require-python]]
+            [libpython-clj2.python :refer [py. py.. py.-] :as py]
+            [scicloj.noj.v1.vis.hanami :as hanami]
+            [aerial.hanami.templates :as ht]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn to-kebab-case [s]
+  (-> s
+    (str/lower-case)
+    (str/replace #" " "-")))
+
+
+(-> "data/nasdaq-instrument-list.csv"
+  (tc/dataset {:key-fn (comp keyword to-kebab-case)})
+  (tc/select-columns [:symbol :name]))
+
+
+(-> "data/nasdaq-instrument-list.csv"
+  (tc/dataset {:key-fn (comp keyword to-kebab-case)})
+  (tc/order-by :volume)
+  (tc/head 10))
+
+(def nasdaq-instruments
+  (-> "data/nasdaq-instrument-list.csv"
+      (tc/dataset {:key-fn (comp keyword to-kebab-case)})))
+
+(-> nasdaq-instruments
+  (tc/group-by [:country])
+  (tc/aggregate {:count tc/row-count})
+  (hanami/plot ht/bar-chart {
+                             :X :country
+                             :Y :count
+                             :XTYPE :nominal
+                             :WIDTH 1000
+                             :YSCALE {:type "log"}}))
+
+(-> "data/instruments/1INCH-USD.csv"
+  (tc/dataset {:key-fn (comp keyword to-kebab-case)})
+  (tc/map-columns :pct-change [:open :close] (fn [open close] (-> close
+                                                                  (- open)
+                                                                  (* 100))))
+  (hanami/plot ht/point-chart {
+                               :X :date
+                               :Y :pct-change
+                               :XTYPE :nominal
+                               :WIDTH 1000
+                               }))
+
+;; TODO
+;; 1. Fix scale of the chart. Make the dots coloured if they are above/below 3/-3
+;; 2. Add any chart with an interactable slider
+;; 3. Tips on staying tidy and organised
+
