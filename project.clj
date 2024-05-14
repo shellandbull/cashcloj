e99a8d2163e35732f7ad049aacee59e2a1bdc1c0
Mario G <mariojgintili@gmail.com>
2024-05-14 20:20:32 +0100

    more markdown

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
+                 [org.scicloj/kindly "4-beta4"]
                  [org.clojure/clojure "1.11.1"]
                  [scicloj/tablecloth "7.029.2"]
                  [org.scicloj/clay "2-beta8"]
--- src/cashcloj/core.clj
+++ src/cashcloj/core.clj
@@ -4,20 +4,58 @@
             [tablecloth.api :as tc]
             [tablecloth.column.api :as tcc]
             [scicloj.clay.v2.api :as clay]
-            [libpython-clj2.require :refer [require-python]]
-            [libpython-clj2.python :refer [py. py.. py.-] :as py]
             [scicloj.noj.v1.vis.hanami :as hanami]
+            [scicloj.kindly.v4.kind :as kind]
             [aerial.hanami.templates :as ht]))
+
+
+(kind/md ["
+# [Scicloj](https://scicloj.github.io/) explorations part 1 | NASDAQ & Cryptocurrency timeseries
+
+This is my first exploration in the world of data science under the hand of Scicloj.
+I decided to give this stack a try due to its [interactive programming](https://docs.cider.mx/cider/usage/interactive_programming.html) capacities.
+This concept was introduced to me during a [London Clojurians](https://londonclojurians.org/) talk [\"From data to insights: Clojure for data deep dive\"](https://www.youtube.com/watch?v=eUFf3-og_-Y).
+
+This file you are looking at is rendered using [Clay](https://github.com/scicloj/clay).
+
+**What's the stack?**
+
+- [Spacemacs](https://www.spacemacs.org/) for my text editor as it comes with plugins for [Clay](https://github.com/scicloj/clay) & [Cider](https://github.com/clojure-emacs/cider)
+- [Clojure](https://clojure.org/) A dynamically typed lisp dialect that calls the JVM home
+- [Tablecloth](https://scicloj.github.io/tablecloth) a `dataframe` like manipulation library. Similar to Pandas
+- [Clay](https://github.com/scicloj/clay) for data visualizations in a dynamic fashion
+
+**What'll be doing**
+
+I downloaded all the instruments from [NASDAQ](https://www.nasdaq.com/) and the top 250 cryptocurrencies from [Binance](https://www.binance.com/). We are going to
+go ahead and explore these datasets.
+
+For the purposes of this first exploration I will keep things extremely simple. We'll visualise the data as a table, perform simple transformations & plot it onto a few simple charts.
+
+**How I'm doing it**
+
+With some help & documentation I have set up my spacemacs so I can cast functions onto either a buffer or Clay.
+This way I can immediately get feedback on each single function I evaluate.
+"])
+
+(kind/md ["
+## Reading a dataset
+
+It's really simple just invoke `tc/dataset` over a URL or filename and you'll immediately get a primitive in which is very
+easy to operate on.
+
+In this example I just select a few columns and keep the top 50
+"])
+
+(-> "data/nasdaq-instrument-list.csv"
+  (tc/dataset {:key-fn (comp keyword to-kebab-case)})
+  (tc/select-columns [:symbol :name])
+  (tc/head 50))
 
 (defn to-kebab-case [s]
   (-> s
     (str/lower-case)
     (str/replace #" " "-")))
-
-(-> "data/nasdaq-instrument-list.csv"
-  (tc/dataset {:key-fn (comp keyword to-kebab-case)})
-  (tc/select-columns [:symbol :name]))
-
 
 (-> "data/nasdaq-instrument-list.csv"
   (tc/dataset {:key-fn (comp keyword to-kebab-case)})
@@ -56,9 +94,4 @@
                                :WIDTH 1000
                                :COLOR {:field :highly-volatile}
                                }))
-
-;; TODO
-;; 1. Fix scale of the chart. Make the dots coloured if they are above/below 3/-3
-;; 2. Add any chart with an interactable slider
-;; 3. Tips on staying tidy and organised
 
