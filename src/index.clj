4be27c4f429e3ca420d574cf2b36063724aa70d6
Mario G <mariojgintili@gmail.com>
2024-05-25 19:29:40 +0100

    proofreading

--- src/index.clj
+++ src/index.clj
@@ -5,29 +5,28 @@
 
 ^:kindly/hide-code
 (kind/md
- "# Welcome to Cashcloj,
+ "# Welcome to Cashcloj
 
-My name is Mario Gintili. You can find me on Github [@shellandbull](https://github.com/shellandbull).
+Find me on Github [@shellandbull](https://github.com/shellandbull) & on [Linkedin](https://www.linkedin.com/in/mario-gintili-software-engineer/)
 
-Data Science, Data Engineering & Machine Learning have always been a personal interest of mine. During my uni years I was an economics major and I had a deep passion for decision modelling alla [Moneyball](https://en.wikipedia.org/wiki/Moneyball_(film)). My inspiration came from films of the sort
+Machine learning & data science have always been an interest of mine. Originally an economics major now turned into a software engineer, I had a deep passion for decision modelling. My inspiration for it came from movies like [Moneyball](https://en.wikipedia.org/wiki/Moneyball_(film)) & [A beautiful mind](https://en.wikipedia.org/wiki/A_Beautiful_Mind_(film))
 
-I'm originally a Rubyist and I was introduced to Clojure long time ago. This year(2024, in case you are reading this in the future) I saw a talk @ [London Clojurians](https://londonclojurians.org/) by the [Scicloj](https://scicloj.github.io/) which showcased a new stack for DS/ML. The stack includes:
+I'm originally a Rubyist, I was introduced to Clojure long time ago, but I never got around using it commercially due to how hard it is to find a Clojure job. This year(2024, in case you are reading this in the future) I saw a talk @ [London Clojurians](https://londonclojurians.org/) by the [Scicloj](https://scicloj.github.io/) which showcased a new stack for DS/ML. The stack includes:
 
-- Using [spacemacs](https://www.spacemacs.org/), a flavour of emacs, for code editing
+- [Spacemacs](https://www.spacemacs.org/), a flavour of emacs, for code editing
 - [Interactive programming](https://docs.cider.mx/cider/usage/interactive_programming.html) practises
 - [Tablecloth](https://scicloj.github.io/tablecloth/) & [Clay](https://github.com/scicloj/clay) for dataset manipulation & visualisation
 - [Quarto](https://quarto.org/) for technical publishing
 
-This site is a documents my journey learning the ML/DS chops
+This humble Quarto journal you see is a wee attempt at documenting my steps towards achieving prowess at modern, applied ML/DS.
 
 Join me on my silly explorations
 
 ## Goals
 
-- Learn to use emacs in evil mode
-- Learn data engineering. The usual stuff like feature engineering, scrubbing data, joining datasets, formulating the right questions
-- Learn Clojure. My background's in OOP dynamically typed languages, this is my first lisp 
-- Learn Scicloj
-
-
+- Learn emacs in evil mode
+- Learn data engineering
+- Learn machine learning
+- Learn Clojure
+- Use the Scicloj stack to achieve all of the above
 ")
